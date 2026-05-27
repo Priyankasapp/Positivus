e88842af237d7bcf6686c375624125ab2e51bdb7
Priyankasapp <priyanka.s@applocum.org>
2026-05-27 13:51:54 +0530

    add animation on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,37 @@
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import ScrollReveal from "scrollreveal";
+
 import { logo } from "../assets/images/images";
 import "../App.css";
+
 function Navbar() {
+
+  useEffect(() => {
+
+    ScrollReveal().reveal(".navbar", {
+      origin: "top",
+      distance: "80px",
+      duration: 1000,
+      delay: 200,
+      reset: false,
+    });
+
+    ScrollReveal().reveal(".nav-item", {
+      origin: "top",
+      distance: "50px",
+      duration: 1000,
+      interval: 150,
+    });
+
+  }, []);
+
   return (
     <nav className="navbar navbar-expand-lg py-4">
+
       <div className="container">
 
-         {/* LOGO */}
+        {/* LOGO */}
         <Link
           to="/"
           className="navbar-brand d-flex align-items-center gap-2"
@@ -73,7 +98,6 @@ function Navbar() {
               </Link>
             </li>
 
-            {/* BUTTON */}
             <li className="nav-item ms-lg-3">
               <button className="btn btn-outline-dark rounded-4 px-4 py-3 fw-medium">
                 Request a quote
